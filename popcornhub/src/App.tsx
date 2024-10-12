400cf21d32023b4c49d96842fc88fcaf1d9ec62b
AmitRaj2001 <amit.12018323@lpu.in>
2024-10-12 10:04:50 +0530

    modified:   popcornhub/src/App.css 	modified:   popcornhub/src/App.tsx 	new file:   popcornhub/src/components/ListGroup.tsx 	deleted:    popcornhub/src/index.css 	modified:   popcornhub/src/main.tsx

--- popcornhub/src/App.tsx
+++ popcornhub/src/App.tsx
@@ -1,6 +1,26 @@
-import Message from './Message';
-function App(){
-  return <div><Message /></div>
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = [
+    "New Yourk",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Philadelphia",
+    "Phoenix",
+  ];
+
+  items = [];
+  return (
+    <>
+      <h1>List</h1>
+      {items.length ===0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key = {item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default App; 
+export default App;
